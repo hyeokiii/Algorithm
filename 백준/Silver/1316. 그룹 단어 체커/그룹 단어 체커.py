cf7de4f4adf98403d4f10c2f4ffcbd63a2ba5bbb
jaehyeok kim <rlawogur0533@gmail.com>
2024-10-18 16:30:29 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,22 +1,13 @@
-num = input()
-items = [] * int(num)
-count = int(num)
-list = []
-for i in range(int(num)):
-    items.append(input())
+n = int(input())
+count = n
 
-for item in items:
-    for i in range(len(item)):
-        if i == len(item)-1 and item[i] not in list:
-             list.append(item[i])
-        elif  i == len(item)-1 and item[i]  in list:
-            count -= 1
+for i in range(n):
+    word=list(input())
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            continue
+        elif word[j] in word[j+1:]:
+            count -=1
             break
-        elif item[i] != item[i+1] and item[i] not in list:
-            list.append(item[i])
-        elif item[i] in list:
-            count -= 1
-            break
-    list.clear()
 
 print(count)
