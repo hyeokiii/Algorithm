830724f10e3473d7bd439e69daf12fc016409efe
jaehyeok kim <rlawogur0533@gmail.com>
2024-10-18 10:47:13 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,7 @@
+str = input()
+list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for i in range(26):
+    list[i] = str.find(list[i])
+
+for i in list:
+    print(i,end=' ')
